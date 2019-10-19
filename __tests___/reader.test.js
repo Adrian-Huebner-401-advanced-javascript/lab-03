a743d7e6d0c5a89a26f27e28d3f58fe54c489c1c
adrianhuebner <adrianh92@gmail.com>
2019-10-19 14:45:18 -0700

    tests added for await and promises

--- __tests___/reader.test.js
+++ __tests___/reader.test.js
@@ -2,23 +2,54 @@
 
 jest.mock('fs');
 
-const fileReader = require('../lib/file-reader-callback');
+const fileCallback = require('../lib/file-reader-callback');
+const filePromises = require('../lib/file-reader-promise');
+const fileAwaits = require('../lib/file-reader-await');
 
-describe('tests file handler', () => {
+// test callbacks
+describe('tests file handler callback', () => {
   it ('writes to a file using a callback', () => {
-    fileReader.writer('test.json', {name: 'Test'}, (err, data) => {
+    fileCallback.writer('test.json', {name: 'Test'}, (err, data) => {
       expect(err).not.toBeDefined();
       expect(data).toBeDefined();
     });
   });
   it('defines err with bad path with callback', () => {
-    fileReader.writer('bad.txt', {name: 'Test'}, (err, data) => {
+    fileCallback.writer('bad.txt', {name: 'Test'}, (err, data) => {
       expect(err).toBe('Invalid File');
       expect(data).not.toBeDefined();
     });
   });
 });
 
-//test promises
+// test promises
+describe('tests file handler promises', () => {
+  it ('writes to a file using a promise', () => {
+    filePromises.writer('test.json', {name: 'Test'}, (err,data) => {
+      expect(err).not.toBeDefined();
+      expect(data).toBeDefined();
+    });
+  });
+  it('defines err with bad path with promise', () => {
+    filePromises.writer('bad.txt', {name: 'Test'}, (err,data) => {
+      expect(err).toBe('Invalid File');
+      expect(data).not.toBeDefined();
+    });
+  });
+});
 
-//test awaits
+// test awaits
+describe('tests file handler await', () => {
+  it('writes to a file using await', () => {
+    fileAwaits.writer('test.json', {name: 'Test'}, (err,data) => {
+      expect(err).not.toBeDefined();
+      expect(data).toBeDefined();
+    });
+  });
+  it('defines err with bad path with await', () => {
+    fileAwaits.writer('bad.txt', {name: 'Test'}, (err,data) => {
+      expect(err).toBe('Invalid File');
+      expect(data).not.toBeDefined();
+    });
+  });
+});
